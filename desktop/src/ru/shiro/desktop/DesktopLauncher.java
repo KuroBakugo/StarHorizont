733e4501ca2e1176f74f0675278f15a2363b0d51
Shiro <demn.red@gmail.com>
2021-08-09 23:55:22 +0300

    Initial commit

---
+++ desktop/src/ru/shiro/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.shiro.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.shiro.StarHorizont;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarHorizont(), config);
+	}
+}
