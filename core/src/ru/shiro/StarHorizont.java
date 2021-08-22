c01c4253d8a059b69baed3bf59b1d4ba6ca807bd
Shiro <demn.red@gmail.com>
2021-08-22 18:08:47 +0300

    lesson1

--- core/src/ru/shiro/StarHorizont.java
+++ core/src/ru/shiro/StarHorizont.java
@@ -1,6 +1,7 @@
 package ru.shiro;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,18 +9,21 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class StarHorizont extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
 	private int x, y = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		background = new Texture("images.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(background ,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
@@ -28,5 +32,6 @@ public class StarHorizont extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		background.dispose();
 	}
 }
